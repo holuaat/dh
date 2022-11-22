96cdde6b3c7ae1faad760e847e48cbebc288856e
holuaat <holuaat@gmail.com>
2022-11-22 11:03:41 +0100

    corrected

--- crawl_ids.py
+++ crawl_ids.py
@@ -1,25 +1,86 @@
-# -*- coding: utf-8 -*-
 import bs4
-import requests
 import json
+import requests
+from pathlib import Path
+import pdb
+from typing import Union
+from argparse import ArgumentParser
 
-def save_ids(url:str):
-    """Takes url as string argument, saves dates and ids to a file named ids.json"""
-    webpage = requests.get(url)
-    soup = bs4.BeautifulSoup(webpage.text, features="lxml")
-    
-    out_list = []
+
+def parse_arguments():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--input_dir', '-d', default="/home/elte-dh-clymene/dh/proba", type=Path,
+                        help='Directory where previous info is stored.')
+    parser.add_argument('--url', '-u', type=str, default="http://google.com",
+                        help='REST API url')
+    args = parser.parse_args()
+    return args
+
+
+months_dict = {
+    'January': '01',
+    'February': '02',
+    'March': '03',
+    'April': '04',
+    'May': '05',
+    'June': '06',
+    'July': '07',
+    'August': '08',
+    'September': '09',
+    'October': '10',
+    'November': '11',
+    'December': '12',
+    'Winter': '12',
+    'Summer': '06',
+    'Spring': '03',
+    'Fall': '09',
+}
+
+
+def get_info_from_line(li: bs4.element.Tag) -> Union[tuple, None]:
+    if "CC-MAIN" in li.text:
+        month_year_str = li.find("a").text
+        month_str, year_str = month_year_str.split(" ")
+        if month_str not in months_dict:
+            month_str = month_str.split("/")[0]
+        month_num_str = months_dict[month_str]
+        date_str = year_str + "-" + month_num_str
+        date_id = "CC-MAIN-" + li.text.split("CC-MAIN-")[1].split("/")[0].split(" ")[0]
+        return date_str, date_id
+    else:
+        return None
+
+
+def get_info_dict(input_dir: Path) -> dict:
+    files_in_dir = [path.name for path in input_dir.iterdir()]
+    webpage = requests.get("https://commoncrawl.org/the-data/get-started/")
+    soup = bs4.BeautifulSoup(webpage.text, features="html5lib")
+
+    out_dict = {}
     for ul in soup.find_all("ul"):
         if "CC-MAIN" in ul.text:
             for idx, li in enumerate(ul.findChildren('li')):
-                if "CC-MAIN" in li.text:
-                    date_id = li.text.split("CC-MAIN-")[1].split("/")[0].split(" ")[0]
-                    out_list.append(date_id)
+                info = get_info_from_line(li)
+                if info is not None:
+                    date_str, date_id = info
+                    if date_str not in files_in_dir:
+                        out_dict[date_str] = date_id
             break
         
-    print(out_list)
-    with open('ids.json', 'w') as f:
-        json.dump(out_list, f)    
+    # print(out_dict)
+    return out_dict
 
-url = "https://commoncrawl.org/the-data/get-started/"
-save_ids(url)
+
+def send_dict_to_url(url: str, info_dict: dict) -> None:
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url, data=json.dumps(info_dict), headers=headers)
+
+
+def main():
+    args = parse_arguments()
+    info_dict = get_info_dict(args.input_dir)
+    send_dict_to_url(args.url, info_dict)
+
+
+if __name__ == '__main__':
+    main()
